96b86cdf383f71256cef44fe7addca62bd584553
thiên lê <thienle255@gmail.com>
2023-08-09 20:33:26 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard/Index');
+});
 //group, prefix: dashboard, name: dashboard.
