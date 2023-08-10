b37007216037d49a97293dc1c8f162fd7623c7d1
Levie <svpv.hotro@gmail.com>
2023-08-10 15:21:55 +0700

    add button

--- resources/views/layouts/dashboard/partials/navbar.blade.php
+++ resources/views/layouts/dashboard/partials/navbar.blade.php
@@ -1,7 +1,10 @@
     <div class="page-main-header">
     <div class="main-header-right row m-0">
         <div class="main-header-left">
-            <div class="logo-wrapper"><a href="#"><img class="img-fluid" src="/assets/images/logo/logo.png" alt=""></a></div>
+            <div class="logo-wrapper">
+                <a href="{{ route('dashboard') }}">
+                    <img class="img-fluid" src="/assets/images/logo/logo.png" alt=""></a>
+                </div>
             <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle"></i></div>
         </div>
         <div class="nav-right col pull-right right-menu p-0">
