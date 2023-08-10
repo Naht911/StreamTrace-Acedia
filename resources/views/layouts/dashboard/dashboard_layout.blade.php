db34dc52c7745cde01835ca3451c36d42d1c6106
Levie <svpv.hotro@gmail.com>
2023-08-10 10:19:17 +0700

    dash layout

--- resources/views/layouts/dashboard/dashboard_layout.blade.php
+++ resources/views/layouts/dashboard/dashboard_layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -10,7 +10,10 @@
      <meta name="author" content="pixelstrap">
      <link rel="icon" href="/assets/images/favicon.png" type="image/x-icon">
      <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
-     <title>viho - Premium Admin Template</title>
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+     <title>@yield('title') | Acedia</title>
+
      <!-- Google font-->
      <link rel="preconnect" href="https://fonts.gstatic.com">
      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
@@ -43,7 +46,7 @@
                     <!-- Container-fluid starts-->
                     <div class="container-fluid dashboard-default-sec">
                          <div class="row">
-                            @yield('content')
+                              @yield('content')
                          </div>
                     </div>
                </div>
