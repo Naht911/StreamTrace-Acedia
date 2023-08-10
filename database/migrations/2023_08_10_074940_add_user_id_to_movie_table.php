56dba5d89b30ace2c59ba69c1135153ef706260e
Levie <svpv.hotro@gmail.com>
2023-08-10 14:50:47 +0700

    thêm cột user id vào movie

---
+++ database/migrations/2023_08_10_074940_add_user_id_to_movie_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movie', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movie', function (Blueprint $table) {
+            //huỷ khóa ngoại
+            $table->dropForeign(['user_id']);
+            //xóa cột
+            $table->dropColumn('user_id');
+        });
+    }
+};
