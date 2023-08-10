ce517a0514b60cd4754000d49a37d0e862af1cd4
Levie <svpv.hotro@gmail.com>
2023-08-10 22:50:28 +0700

    viết lại migrate

--- database/migrations/2023_08_09_084530_create_movie_provider_table.php
+++ database/migrations/2023_08_09_084530_create_movie_provider_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('movie_provider', function (Blueprint $table) {
             $table->id();
             $table->foreignId('movie_id')->constrained('movie')->onDelete('cascade');
-            $table->foreignId('provider_id')->constrained('streaming_service_provider')->onDelete('cascade');
+            $table->foreignId('streaming_service_provider_id')->constrained('streaming_service_provider')->onDelete('cascade');
             $table->integer('price')->comment('unit: usd, cent');
             $table->foreignId('type_price_id')->constrained('type_price')->onDelete('cascade');
             $table->foreignId('movie_resolution_id')->constrained('movie_resolution')->onDelete('cascade');
