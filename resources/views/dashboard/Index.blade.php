d8c2d3cd006e09a846982ab675f43619d7b382f0
Levie <svpv.hotro@gmail.com>
2023-08-10 09:55:46 +0700

    fix cấu trúc Chỉnh dashboard

--- resources/views/dashboard/Index.blade.php
+++ resources/views/dashboard/Index.blade.php
@@ -1,15 +1,43 @@
 @extends('layouts.dashboard.dashboard_layout')
-
-@section('title')
-     <title>Admin</title>
-@endsection
-@section('css')
-@endsection
+@section('title', 'Dashboard')
+@push('css')
+@endpush
 
 
 @section('content')
-     <p>content.</p>
+
+
+     <!-- Container-fluid starts-->
+     <div class="container-fluid dashboard-default-sec">
+          <div class="row">
+
+
+               <div class="col-xl-12 box-col-12 des-xl-100 top-dealer-sec">
+                    <div class="card">
+                         <div class="card-header pb-0">
+                              <div class="header-top d-sm-flex justify-content-between align-items-center">
+                                   <div class="center-content">
+                                        <h3 class="text-capitalize">Kiểm Tra Và Tố Cáo Lừa Đảo Qua mạng!</h3>
+                                        <p class="d-sm-flex align-items-center">
+                                             <span class="m-r-10">Giúp bạn an toàn hơn trên không gian mạng</span>
+                                        </p>
+                                   </div>
+
+
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+
+
+          </div>
+     </div>
+
+
+
 @endsection
 
-@section('js')
-@endsection
+<!-- Container-fluid Ends-->
+@push('scripts')
+@endpush
---
+++ resources/views/layouts/dashboard/partials/js.blade.php
@@ -0,0 +1,24 @@
+ <!-- latest jquery-->
+ <script src="/assets/js/jquery-3.5.1.min.js"></script>
+ <!-- feather icon js-->
+ <script src="/assets/js/icons/feather-icon/feather.min.js"></script>
+ <script src="/assets/js/icons/feather-icon/feather-icon.js"></script>
+ <!-- Sidebar jquery-->
+ <script src="/assets/js/sidebar-menu.js"></script>
+ <script src="/assets/js/config.js"></script>
+ <!-- Bootstrap js-->
+ <script src="/assets/js/bootstrap/popper.min.js"></script>
+ <script src="/assets/js/bootstrap/bootstrap.min.js"></script>
+ <!-- Plugins JS start-->
+
+
+ <script src="/assets/js/counter/jquery.waypoints.min.js"></script>
+ <script src="/assets/js/counter/jquery.counterup.min.js"></script>
+ <script src="/assets/js/counter/counter-custom.js"></script>
+
+ <!-- Plugins JS Ends-->
+ <!-- Theme js-->
+ <script src="/assets/js/script.js"></script>
+ @yield('js')
+ <!-- login js-->
+ <!-- Plugin used-->
