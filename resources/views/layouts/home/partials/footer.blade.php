e2a9c61ff1a3a988f434160dd15594d491a93df7
Levie <svpv.hotro@gmail.com>
2023-08-13 10:49:58 +0700

    chữ

--- resources/views/layouts/home/partials/footer.blade.php
+++ resources/views/layouts/home/partials/footer.blade.php
@@ -1,8 +1,36 @@
 <footer>
     <div class="container">
-         <div class="footer-left">Acedia | The Streaming Guide</div>
-         <div class="footer-right">
-              © 2023 Acedia - All external content remains the property of the rightful owner.
-         </div>
+        <div class="center">
+            <div class="detail-1">
+                <h3>About Us</h3>
+                <p>Acedia is ......</p>
+            </div>
+            <div class="detail-2">
+                <h3>Contact Us</h3>
+                <p><i class="fa-solid fa-phone"></i> 000 - 000 - 0000</p>
+                <p><i class="fa-solid fa-envelope"></i>acedia@support.com</p>
+                <p><i class="fa-solid fa-location-dot"></i>CMT</p>
+            </div>
+            <div class="detail-3">
+                <h3>Subscribe Us</h3>
+                <form action="#">
+                    <input type="text" placeholder="Enter email address" />
+                    <input type="submit" name="" value="Send" />
+                    <div class="media-icons">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="footer-left">Acedia | The Streaming Guide</div>
+            <div class="footer-right">
+                © 2023 Acedia (3.6.1) - All external content remains the property of the rightful owner.
+            </div>
+        </div>
     </div>
 </footer>
