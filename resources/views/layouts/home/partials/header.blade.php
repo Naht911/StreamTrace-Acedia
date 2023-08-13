b80eef68dcdf28b7ab5e461e7012954481dd42db
phuslee24 <tranphu.thpthht@gmail.com>
2023-08-13 10:19:21 +0700

    feedback_ok

--- resources/views/layouts/home/partials/header.blade.php
+++ resources/views/layouts/home/partials/header.blade.php
@@ -1,58 +1,54 @@
 <div class="header mb-3">
-     <div class="container">
-          <div class="header-left">
-               <a href="{{ route('home') }}" class="header-logo">
-                    <img src="{{ asset('img/acedia.png') }}" alt="" />
-               </a>
-          </div>
-          <div class="header-right">
-               <div class="items">
-                    <li><a href="{{ route('home') }}" class="{{ routeActive('home') }}">Home</a></li>
-                    <li><a href="{{ route('new') }}" class="{{ routeActive('new') }}">New</a></li>
-                    <li><a href="{{ route('popular') }}" class="{{ routeActive('popular') }}">Popular</a></li>
-                    <li><a href="{{ route('home.wathchlist') }}" class="{{ routeActive('home.wathchlist') }}">Watchlist</a></li>
-               </div>
-               <li class="search-icon">
-                    
-                    
-                    <form action="{{route('search_movie')}}" method="GET">
-                         <div class="form-group">
-                              <label class="icon">
-                                   {{-- <span class="fas fa-search"></span> --}}
-                                   <br>
-                              </label>
-                         <input type="text" placeholder="Search for movies" name="search_movie" autofocus style="margin-left: 15px"/>
-                         <button type="submit" name="search" class="search"></button>  
-                         </div>
-                    </form>
-               </li>
-               @if (!Auth::check())
-                    <div class="buttons">
-                         <a href="{{ route('login') }}">Sign In</a>
+    <div class="container">
+        <div class="header-left">
+            <a href="{{ route('home') }}" class="header-logo">
+                <img src="{{ asset('img/acedia.png') }}" alt="" />
+            </a>
+        </div>
+        <div class="header-right">
+            <div class="items">
+                <li><a href="{{ route('home') }}" class="{{ routeActive('home') }}">Home</a></li>
+                <li><a href="{{ route('new') }}" class="{{ routeActive('new') }}">New</a></li>
+                <li><a href="{{ route('popular') }}" class="{{ routeActive('popular') }}">Popular</a></li>
+                <li><a href="{{ route('home.wathchlist') }}" class="{{ routeActive('home.wathchlist') }}">Watchlist</a>
+                </li>
+            </div>
+            <li class="search-icon">
+                <label class="icon">
+                    <span class="fas fa-search"></span>
+                </label>
+                <input type="text" placeholder="Search for movies" />
+            </li>
+            @if (!Auth::check())
+                <div class="buttons">
+                    <a href="{{ route('login') }}">Sign In</a>
+                </div>
+            @endif
+
+            <li class="toggleBtn">
+                <span class="fas fa-bars"></span>
+                <div class="detail">
+                    @if (Auth::check())
+                        <div class="row text-white">
+                            <a href="faq.html"> Profile </a>
+                        </div>
+                        <div class="row text-white">
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit" class="text-dark">Log Out</button>
+                            </form>
+                        </div>
+                    @endif
+                    <div class="row text-white">
+                        <a href="{{ route('faq') }}"> FAQ </a>
                     </div>
-               @endif
-
-               <li class="toggleBtn">
-                    <span class="fas fa-bars"></span>
-                    <div class="detail">
-                         @if (Auth::check())
-                              <div class="row text-white">
-                                   <a href="faq.html"> Profile </a>
-                              </div>
-                              <div class="row text-white">
-                                   <form action="{{ route('logout') }}" method="POST">
-                                        @csrf
-                                        <button type="submit" class="text-dark">Log Out</button>
-                                   </form>
-                              </div>
-                         @endif
-                         <div class="row text-white">
-                              <a href="{{route('faq')}}"> FAQ </a>
-                         </div>
+                    <div class="row text-white">
+                        <a href="{{ route('feedback') }}"> Feedback </a>
                     </div>
-               </li>
+                </div>
+            </li>
 
 
-          </div>
-     </div>
+        </div>
+    </div>
 </div>
